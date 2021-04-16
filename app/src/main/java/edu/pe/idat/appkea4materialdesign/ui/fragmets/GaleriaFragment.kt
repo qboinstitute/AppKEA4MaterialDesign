fa5d0349442fba0827d60c729e2355a0321174e2
LuisAngel <luisangel.salvat@gmail.com>
2021-04-15 22:16:39 -0500

    5ta. parte del ejercicio, recyclerview en fragment.

--- app/src/main/java/edu/pe/idat/appkea4materialdesign/ui/fragmets/GaleriaFragment.kt
+++ app/src/main/java/edu/pe/idat/appkea4materialdesign/ui/fragmets/GaleriaFragment.kt
@@ -5,18 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import edu.pe.idat.appkea4materialdesign.R
+import edu.pe.idat.appkea4materialdesign.adapter.AndroidAdapter
+import edu.pe.idat.appkea4materialdesign.databinding.FragmentGaleriaBinding
+import edu.pe.idat.appkea4materialdesign.service.DatosService
 
 
 class GaleriaFragment : Fragment() {
+
+    private var _binding: FragmentGaleriaBinding? = null
+    private val binding get() = _binding!!
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_galeria, container, false)
+        _binding = FragmentGaleriaBinding.inflate(inflater, container, false)
+        binding.rvandroidfrag.adapter = AndroidAdapter(
+            DatosService().listaVersionesAndroid(), requireActivity())
+        binding.rvandroidfrag.layoutManager = LinearLayoutManager(
+            requireActivity())
+        return binding.root
     }
 
 }
